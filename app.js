5cb28336c6a1d60e719165d916760a3026b8b4e9
KALEEMULLAH <160345037+Kaleemullah3095@users.noreply.github.com>
2024-03-08 23:10:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var names = ["IMRAN", "INAYAT", "IRFAN", "ALIYAN"];
+names.forEach(function (name) { console.log(name); });
---
+++ app.ts
@@ -0,0 +1,2 @@
+var names: string[]=["IMRAN","INAYAT","IRFAN","ALIYAN"];
+names.forEach(name =>{console.log(name)});
